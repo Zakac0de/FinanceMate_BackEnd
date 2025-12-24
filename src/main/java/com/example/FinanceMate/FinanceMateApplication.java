23cc8e06d809b62e2b9c6e04117d2af684bb7601
Zakac0de <sakariyeaden@gmail.com>
2025-12-24 15:01:25 +0200

    Initialize FinanceMate project with Spring Boot scaffolding

---
+++ src/main/java/com/example/FinanceMate/FinanceMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.FinanceMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FinanceMate/FinanceMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FinanceMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
